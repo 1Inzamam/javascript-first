9a4c93b7740b40e90723ac5a4f95989dddd55108
1Inzamam <inzamamkhan618@gmail.com>
2024-09-26 08:01:51 +0600

    using typeof to see the type of the variable

--- variable.js
+++ variable.js
@@ -5,3 +5,10 @@ var height = 6;
 var price = 200;
 var balance = 2000;
 var days = 15; 
+
+ var carname;
+ carname = 'GT-R';
+ console.log(carname)
+ console.log(balance)
+ 
+ console.log(typeof price)

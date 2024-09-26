7f06b4387d4dfc5264886dd65ce31923680ec45d
1Inzamam <inzamamkhan618@gmail.com>
2024-09-26 09:07:39 +0600

    simple addition and subtraction

---
+++ market-task.js
@@ -0,0 +1,18 @@
+// total budget 
+var b = 1200;
+console.log("Total budget = "+b)
+
+var onion = 200;
+console.log(" Price of onion = " + "$" + onion)
+
+var tomato =400;
+console.log(" Price of tomato = " + "$" + tomato)
+
+var carrot = 300;
+console.log(" Price of carrot = " + "$" + carrot)
+
+var t = onion + tomato + carrot;
+console.log(" Total cost of onion,tomato,carrot = " + "$" + t)
+
+l = b - t;
+console.log("Left budget = " + "$" + l )

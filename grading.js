d9eaffcdb47640131f95862cee18ebe96039e76b
1Inzamam <inzamamkhan618@gmail.com>
2024-09-27 07:56:19 +0600

    grading

---
+++ grading.js
@@ -0,0 +1,22 @@
+var marks = 88;
+if(marks<60){
+    console.log("Grade 'F'")
+}
+else{
+    if(marks>=60 && marks<70){
+        console.log("Grade 'D'")
+    }
+    else{
+        if(marks>=70 && marks<80){
+            console.log("Grade 'C'")
+        }
+        else{
+            if(marks>=80 && marks<90){
+                console.log("Grade 'B'")
+            }
+            else{
+                console.log("Grade 'A'")
+            }
+        }
+    }
+}

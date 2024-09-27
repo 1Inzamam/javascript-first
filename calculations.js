489124ec2e35c7d69f8e9a93e1ceae193a200690
1Inzamam <inzamamkhan618@gmail.com>
2024-09-27 08:23:26 +0600

    calculations with ternary operators

---
+++ calculations.js
@@ -0,0 +1,13 @@
+var num1 = 4;
+var num2 = 4;
+var result;
+// if(num1>num2){
+//     result=num1*2;
+//     console.log(result)
+// }
+// else{
+//     result=num1+num2;
+//     console.log(result)
+// }
+
+ num1>num2 ? console.log(result=(num1*2)):console.log(result=(num1+num2))

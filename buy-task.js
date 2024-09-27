8d1db856f828f20c99b2b50f7a1234b00dfad869
1Inzamam <inzamamkhan618@gmail.com>
2024-09-27 07:04:19 +0600

    buy conditions

---
+++ buy-task.js
@@ -0,0 +1,9 @@
+var burger = 500;
+if(burger>500){
+    cokePrice=0;
+    console.log("You have a free coke (Price): " + cokePrice + "tk")
+}
+else{
+    cokePrice = 25;
+    console.log("your coke price is : " + cokePrice +"tk")
+}
--- conditions-discount.js
+++ conditions-discount.js
@@ -1,8 +1,14 @@
-const price = 8000;
+const price = 5000;
 if(price>= 7000)
 {
     const discount = price*10/100;
     const amount = price - discount;
+    console.log(amount);
+}
+else if(price>=2000)
+{
+    discount = price*5/100;
+    amount = price -discount;
     console.log(amount);
 }
 else{

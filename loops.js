c82467fd75e7963167026ad45d07aa3d8a285fb1
1Inzamam <inzamamkhan618@gmail.com>
2024-09-29 09:57:54 +0600

    even number using for loop and continue

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-// for loop 
+// for-of loop 
 
 // let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
@@ -28,10 +28,37 @@
 
 // odd number
 
-let num = 1;
-while(num<=10){
-    if(num % 2 != 0){
-        console.log("odd number : ", num);
+// let num = 1;
+// while(num<=10){
+//     if(num % 2 != 0){
+//         console.log("odd number : ", num);
+//     }
+//     num++;
+// }
+
+
+// for loop
+// let sum = 0;
+// for(let num = 0; num <= 100; num++){
+//     console.log("Numbers : ", num);
+//     if(num >= 50){
+//         break;
+//     }
+//     else{
+//         sum =num + sum;
+//     console.log("Summation of num and sum : ", sum);
+//     }
+    
+// }
+
+// even number using for loop and  continue 
+
+for (let num = 0; num<=10; num++){
+    if(num%2==1){
+        continue;
     }
-    num++;
+    else{
+        console.log(num);
+    }
 }
+

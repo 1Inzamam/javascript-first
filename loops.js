a36e74a8ec4a012285893cb5d3858211478a54f9
1Inzamam <inzamamkhan618@gmail.com>
2024-09-29 08:41:08 +0600

    while loop

--- loops.js
+++ loops.js
@@ -1,20 +1,20 @@
 // for loop 
 
-let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-for(let num2 of num1){
-    console.log(num2);
-}
+// for(let num2 of num1){
+//     console.log(num2);
+// }
 
 
 // while loop
 
-// let num2 = 1;
-// while(num2 <= 10)
-// {
-//     console.log(num2);
-//     num2++;
-// }
+let num2 = 1;
+while(num2 <= 10)
+{
+    console.log(num2);
+    num2++;
+}
 
 // even number with while loop
 

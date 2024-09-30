5876c650ae8a9912a65bde3138b3383eda77c51e
1Inzamam <inzamamkhan618@gmail.com>
2024-09-30 08:30:11 +0600

    use of join

--- string.js
+++ string.js
@@ -1,21 +1,43 @@
+
+
+
+
 // to compare strings we should always convert them to lowercase or uppercase
 
-const a = 'female';
-const b = 'Female';
-if(a.toLowerCase == b.toLowerCase){
-    console.log('same gender');
-}else{
-    console.log("different gender");
-}
+
+// const a = 'female';
+// const b = 'Female';
+// if(a.toLowerCase == b.toLowerCase){
+//     console.log('same gender');
+// }else{
+//     console.log("different gender");
+// }
+
+/////////////////////////////////////////////////////////////////////////////
+
 
 // use of slice
 
-const c = "chamber";
-const d = c.slice(3,6);
-console.log(d);
+// const c = "chamber";
+// const d = c.slice(3,6);
+// console.log(d);
+
+
+//////////////////////////////////////////////////////////////////////////////
+
 
 //use of split
 
-console.log(c.split(''));
+// console.log(c.split(''));
 
 // console.log(c.split());
+
+//////////////////////////////////////////////////////////////////////////
+
+// use of join
+
+const e = "amanda, fiora, mozila, alex, adam, rowan";
+const f = e.split(",");
+console.log(f);
+g = f.join(' ');
+console.log(g);

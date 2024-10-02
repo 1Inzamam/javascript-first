20373e8564bae258ba2105ab2d8063d7b1774af8
1Inzamam <inzamamkhan618@gmail.com>
2024-10-02 09:15:22 +0600

    sort array in java script

--- sort.js
+++ sort.js
@@ -1,3 +1,5 @@
 const numbers = [1, 32, 65, 64, 33, 22, 11, 19];
 const numbers_a = numbers.sort(function(a,b){return a-b});
 console.log(numbers_a);
+const numbers_d = numbers.sort(function(a,b){return b-a});
+console.log(numbers_d);

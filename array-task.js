50c36febef3627af46174eb063c52412434cc1cf
1Inzamam <inzamamkhan618@gmail.com>
2024-10-02 09:47:55 +0600

    EVEN numbers of an array of numbers

--- array-task.js
+++ array-task.js
@@ -1,7 +1,5 @@
-const color = ['red', 'blue', 'yellow', 'green', 'indigo'];
+const color = ["red", "blue", "yellow", "green", "indigo"];
 const reversed = [];
-
-
 
 // for(let i=color.length -1; i>=0; i--){
 //     const re_num = color[i];
@@ -9,16 +7,12 @@ const reversed = [];
 // }
 // console.log(reversed);
 
-
 //////////////////////////////////////////////////////////////
-
 
 // for(const num of color){
 //     reversed.unshift(num);
 // }
 // console.log(reversed);
-
-
 
 ///////////////////////////////////////////////////////
 
@@ -27,3 +21,18 @@ const reversed = [];
 //     reversed.unshift(num);
 // }
 // console.log(reversed);
+
+//////////////////////////EVEN NUMBER///////////////////////////////////////////////////
+
+const array = [11, 9, 12, 43, 7, 8, 22, 2, 1, 99, 100];
+const final_array = [];
+for (let i = 0; i < array.length; i++) {
+  const num = array[i];
+  if (num % 2 == 0) {
+    final_array.push(num);
+  }
+}
+const final_as = final_array.sort(function (a, b) {
+  return a - b;
+});
+console.log(final_as);

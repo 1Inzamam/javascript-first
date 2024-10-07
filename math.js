12a79693d014b9dfd09d38684bb4806f4a8b503c
1Inzamam <inzamamkhan618@gmail.com>
2024-10-07 11:06:04 +0600

    finding minimum value creating a function

--- math.js
+++ math.js
@@ -1,2 +1,22 @@
+////////////////////FINDING MAX USING MATH OBJECT /////////////////////////////////
+
 const max = Math.max(1,2,3,4,5,6);
 console.log(max)
+
+
+
+/////////////////////////FINDING MINIMUM VALUE CREATING A FUNCTION////////////////
+
+const array = [11, 12, 1, 2, 3, 4, 0, 5, 6, 7, 8, 9, 10];
+function min(num){
+    let out = num[0];
+    for(let number of num){
+        if(number < out){
+            out = number;
+        }
+    }
+    return out;
+}
+
+const minimum = min(array);
+console.log(minimum);

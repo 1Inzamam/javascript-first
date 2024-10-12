07f24c7d19fa82cb5dd458c062229926150dfb06
1Inzamam <inzamamkhan618@gmail.com>
2024-10-12 15:15:33 +0600

    money calculator fix

--- money-task.js
+++ money-task.js
@@ -1,7 +1,12 @@
 function calculation(num){
-   const income = num *120;
-   leftMoney = income - 900;
-   return leftMoney;
+   if(num < 0){
+      console.log("Incorrect value");
+   }
+   else{
+      const income = num *120;
+      leftMoney = income - 900;
+      return leftMoney;
+   }
 }
 const totalTicket = 12;
 const totalIncome = calculation(totalTicket);
